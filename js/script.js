29149cbb95e4fa83320365f8f5f2124e3e456279
floatomar <156109712+floatomar@users.noreply.github.com>
2025-09-26 22:10:49 +0300

    Update Custom cursor

--- js/script.js
+++ js/script.js
@@ -87,3 +87,9 @@ function downloadResume() {
     link.click();
     document.body.removeChild(link);
 }
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+    cursor.style.top = `${e.clientY}px`;
+    cursor.style.left = `${e.clientX}px`;
+});
